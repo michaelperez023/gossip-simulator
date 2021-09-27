07aee027031aad6d7bc170a5d9c51ee1a936d5b2
Michael Perez <40220887+michaelperez023@users.noreply.github.com>
2021-09-26 21:35:32 -0400

    added command line inputs, started on boss and node actors

--- Project2.fsx
+++ Project2.fsx
@@ -1,4 +1,4 @@
-// Author: Michael Perez UFID: 62408470
+// Authors: Blas Kojusner and Michael Perez UFID: 62408470
 // Distributed Operating Systems - COP5615
 #time "on"
 #r "nuget: Akka.FSharp"
@@ -7,29 +7,102 @@ open System
 open System.Diagnostics
 open Akka.Actor
 open Akka.FSharp
-open System.Text
-open System.Security.Cryptography
 
-let BossActor (mailbox:Actor<_>) =
+let system = ActorSystem.Create("System")
+let timer = Stopwatch()
 
-    let rec loop () = actor {
+type Messages =
+    | StartGossip of String
+    | StartPushSum of String
+
+let Boss (mailbox:Actor<_>) =
+    let rec loop() = actor {
+            let! message = mailbox.Receive()
+            match message with
+
+            | _->()
+
+            return! loop()
+        }
+    loop()
+
+let Node listener numNodes (mailbox:Actor<_>)  =
+    let rec loop() = actor {
         let! message = mailbox.Receive()
-        let sender = mailbox.Sender()
-
         match message with
-        | BossInput(n) ->
-        | _ -> ()
-        return! loop ()
+        | StartGossip msg ->
+            printfn "starting gossip..."
+
+
+        | StartPushSum msg ->
+            printfn "starting push sum..."
+
+
+        | _-> ()
+        return! loop()
     }
-    loop ()
+    loop()
+
+let start algo numNodes nodeArray =
+            (nodeArray : _ array)|>ignore
+            let r = System.Random()
+
+            if algo = "gossip" then
+                let starter = r.Next(0, numNodes-1)
+                nodeArray.[starter] <! StartGossip("Hello")
+            elif algo = "push-sum" then
+                let starter = r.Next(0, numNodes-1)
+                nodeArray.[starter] <! StartPushSum("Hello")
+            else
+                printfn "Wrong Algorithm Argument!"
+
+let buildFull numNodes algo =
+    printfn "Full network with %i nodes and algorithm %s" numNodes algo
+    let boss = Boss |> spawn system "boss"
+    let nodeArray = Array.zeroCreate(numNodes)
+
+    timer.Start()
+    start algo numNodes nodeArray
+
+let build3dGrid numNodes algo =
+    printfn "3D grid with %i nodes and algorithm %s" numNodes algo
+    let boss = Boss |> spawn system "boss"
+    let nodeArray = Array.zeroCreate(numNodes)
+
+    timer.Start()
+    start algo numNodes nodeArray
+
+let buildLine numNodes algo =
+    printfn "Line with %i nodes and algorithm %s" numNodes algo
+    let boss = Boss |> spawn system "boss"
+    let nodeArray = Array.zeroCreate(numNodes)
+
+    timer.Start()
+    start algo numNodes nodeArray
+
+let buildImp3d numNodes algo =
+    printfn "Imperfect 3d grid build with %i nodes and algorithm %s" numNodes algo
+    let boss = Boss |> spawn system "boss"
+    let nodeArray = Array.zeroCreate(numNodes)
+
+    timer.Start()
+    start algo numNodes nodeArray
 
 match fsi.CommandLineArgs.Length with
-| 2 ->
-    let zeros = fsi.CommandLineArgs.[1] |> int
+| 4 ->
+    let args : string array = fsi.CommandLineArgs |> Array.tail
+    let numNodes = args.[0] |> int
+    let topology = args.[1] |> string
+    let algo = args.[2] |> string
 
-    let bossRef = spawn system "boss" BossActor
-    bossRef <! BossInput(zeros)
+    if topology="full" then buildFull numNodes algo
+    elif topology="3D" then build3dGrid numNodes algo
+    elif topology="line" then buildLine numNodes algo
+    elif topology="imp3D" then buildImp3d numNodes algo
+    else printfn "Wrong Topology Argument!"
 
-    system.WhenTerminated.Wait()
+    //System.Console.ReadLine() |> ignore
+
+    //system.WhenTerminated.Wait()
     ()
-| _ -> failwith "You must enter one number as input: i.e. 'dotnet fsi Project1.fsx 2'"
+| _ -> failwith "Error, run program with command: 'dotnet fsi Project2.fsx <numNodes> <topology> <algorithm>'"
